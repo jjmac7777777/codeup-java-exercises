60a54b9e711bdc33ceb76e3ad06764a4be91b313
johnmccloskey <john.j.mccloskey76@gmail.com>
2021-08-28 20:13:50 -0500

    Completed Methods Exercise 8-28-21

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,4 +1,4 @@
-public class MethodsExercises{
+public class MethodsExercises {
 
     public static double addMethod(double numOne, double numTwo){
         return numOne + numTwo;
@@ -6,11 +6,44 @@ public class MethodsExercises{
     }
 
     public static void main(String[] args) {
-    int numOne = 5;
-    int numTwo = 7;
-        System.out.println(addMethod(numOne, numTwo));
+
+        System.out.println(addMethod(5, 7));
+        System.out.println(addMethod(12.7, 3.3));
+        System.out.println(addMethod(39.9, 2.4));
+        System.out.println(addMethod(25.5, 7.7));
+        System.out.println(addMethod(15.4, 8.8));
 
         }
+
+
+    public static double Subtraction(double numOne, double numTwo){
+        return numOne - numTwo;
+    }
+
+    public static double Multiplication(double numOne, double numTwo){
+
+
+        double totalAdded = 0;
+
+        for (int i = 0; i < numTwo; i++){
+            totalAdded += numOne;
+        }
+
+        return totalAdded;
+    }
+
+    public static double Division(double numOne, double numTwo){
+        return numOne / numTwo;
+    }
+
+    public static double Modulus(double numOne, double numTwo){
+
+        if (numTwo == 0){
+            return numOne;
+        }
+
+        return numOne % numTwo;
+    }
 
     }
 
